45a595e5137b425610f0b393e5e2193cc18a06c2
Dmitriii-K <dmitriii.kirilenko@yandex.by>
2024-01-21 20:13:42 +0300

    gid commit

--- nextpractic/js/akertprogram.js
+++ nextpractic/js/akertprogram.js
@@ -1,40 +1,53 @@
-const word0 = "Wassup";
-const word0translation = "Здарова";
-const word1 = "Bye";
-const word1translation = "Покеда";
-const word2 = "Programmer";
-const word2translation = "Программист";
+const words = {
+  word0: {
+    original: "Wassup",
+    translation: "Здарова",
+  },
+  word1: {
+    original: "Bye",
+    translation: "Покеда",
+  },
+  word2: {
+    original: "Programmer",
+    translation: "Программист",
+  },
+  wordsCount: 3,
+};
+const resultMessager = {
+  finishSuccess: "Молодец, Good result",
+  finishUnSuccess: "Молодец, но постарайся лучше",
+};
 
-const finishSuccessMessage = "Молодец, Good result";
-const finishUnSuccessMessage = "Молодец, но постарайся лучше";
+const setting = {
+  correctAnswersMiPaccent: 50,
+};
+const result = {
+  correctAnswerscount: 0,
+};
 
-const wordsCount = 3;
-const correctAnswersMiPaccent = 50;
-
-let correctAnswerscount = 0;
-
-const userAbswer0 = prompt(word0);
-alert(userAbswer0 === word0translation);
-if (userAbswer0 === word0translation) {
-  correctAnswerscount = correctAnswerscount + 1;
+const userAbswer0 = prompt(words.word0.original);
+alert(userAbswer0 === words.word0.translation);
+if (userAbswer0 === words.word0.translation) {
+  result.correctAnswerscount = result.correctAnswerscount + 1;
 }
 
-const userAbswer1 = prompt(word1);
-alert(userAbswer1 === word1translation);
-if (userAbswer1 === word1translation) {
-  correctAnswerscount = correctAnswerscount + 1;
+const userAbswer1 = prompt(words.word1.original);
+alert(userAbswer1 === words.word1.translation);
+if (userAbswer1 === words.word1.translation) {
+  result.correctAnswerscount = result.correctAnswerscount + 1;
 }
 
-const userAbswer2 = prompt(word2);
-alert(userAbswer2 === word2translation);
-if (userAbswer2 === word2translation) {
-  correctAnswerscount = correctAnswerscount + 1;
+const userAbswer2 = prompt(words.word2.original);
+alert(userAbswer2 === words.word2.translation);
+if (userAbswer2 === words.word2.translation) {
+  result.correctAnswerscount = result.correctAnswerscount + 1;
 }
 
-const userCorrectAnswersParcent = (correctAnswerscount / wordsCount) * 100;
+const userCorrectAnswersParcent =
+  (result.correctAnswerscount / words.wordsCount) * 100;
 
-if (userCorrectAnswersParcent > correctAnswersMiPaccent) {
-  alert(finishSuccessMessage);
+if (userCorrectAnswersParcent > setting.correctAnswersMiPaccent) {
+  alert(resultMessager.finishSuccess);
 } else {
-  alert(finishUnSuccessMessage);
+  alert(resultMessager.finishUnSuccess);
 }
