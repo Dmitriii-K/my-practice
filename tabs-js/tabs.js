a8ccf6a5ef8808cf85a48f48dd7e8802d8d404d2
Dmitriii-K <152644174+Dmitriii-K@users.noreply.github.com>
2024-02-02 22:19:51 +0300

    Add files via upload

---
+++ tabs-js/tabs.js
@@ -0,0 +1,28 @@
+const tabsBtn = document.querySelectorAll(".tabs__nav-btn");
+const tabsItems = document.querySelectorAll(".tabs__item");
+
+tabsBtn.forEach(onTabClick);
+
+function onTabClick(item) {
+  item.addEventListener("click", function () {
+    let currentBtn = item;
+    let tabId = currentBtn.getAttribute("data-tab");
+    let currentTab = document.querySelector(tabId);
+
+    if (!currentBtn.classList.contains("active")) {
+      tabsBtn.forEach(function (item) {
+        item.classList.remove("active");
+      });
+
+      tabsItems.forEach(function (item) {
+        item.classList.remove("active");
+      });
+
+      currentBtn.classList.add("active");
+      currentTab.classList.add("active");
+    }
+  });
+}
+
+document.querySelector(".tabs__nav-btn").click(); // Что бы не указывать класс active в html в "tabs__item active"
+// document.querySelector(".tabs__nav-btn:nth-child(2)").click(); // Делает 2 сылку активной по умолчанию
