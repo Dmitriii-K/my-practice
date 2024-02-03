e09576ec4e844684fb32737dd822431429f91a0a
Dmitriii-K <dmitriii.kirilenko@yandex.by>
2024-02-03 18:24:50 +0300

    git commit

--- BattleOfSea/NewBattle/battle.js
+++ BattleOfSea/NewBattle/battle.js
@@ -17,26 +17,31 @@ const view = {
 // view.displayMiss("23");
 
 // Модель поведения
-let model = {
-  bordSize: 7, // размер игрового поля
+var model = {
+  boardSize: 7, // размер игрового поля
   numShips: 3, // кол-во кораблей в игре
   shipLength: 3, // длинна корабля в клетках
   shipsSunk: 0, // кол-во потопленных кораблей на начало игры
 
   ships: [
-    (ship1 = { location: ["10", "20", "30"], hits: ["", "", ""] }),
-    (ship2 = { location: ["32", "33", "34"], hits: ["", "", ""] }),
-    (ship3 = { location: ["63", "64", "65"], hits: ["", "", ""] }),
+    { locations: [0, 0, 0], hits: ["", "", ""] },
+    { locations: [0, 0, 0], hits: ["", "", ""] },
+    { locations: [0, 0, 0], hits: ["", "", ""] },
   ],
+
   fire: function (guess) {
-    // получает координаты выстрела
-    for (let i = 0; i < this.numShips; i++) {
-      let ship = this.ships[i];
-      let index = ship.location.indexOf(guess);
-      if (index >= 0) {
+    for (var i = 0; i < this.numShips; i++) {
+      var ship = this.ships[i];
+      var index = ship.locations.indexOf(guess);
+
+      if (ship.hits[index] === "hit") {
+        view.displayMessage("Вы уже попали в это место!");
+        return true;
+      } else if (index >= 0) {
         ship.hits[index] = "hit";
         view.displayHit(guess);
-        view.displayMessage("Есть попадание!!!");
+        view.displayMessage("Есть попадание!");
+
         if (this.isSunk(ship)) {
           view.displayMessage("Ты потопил мой корабль!");
           this.shipsSunk++;
@@ -48,13 +53,67 @@ let model = {
     view.displayMessage("Ты промазал!");
     return false;
   },
+
   isSunk: function (ship) {
-    for (let i = 0; i < this.shipLength; i++) {
+    for (var i = 0; i < this.shipLength; i++) {
       if (ship.hits[i] !== "hit") {
         return false;
       }
     }
     return true;
+  },
+  // Метод создает в модели массив ships
+  generateShipLocations: function () {
+    var locations;
+    for (var i = 0; i < this.numShips; i++) {
+      do {
+        locations = this.generateShip();
+      } while (this.collision(locations));
+      this.ships[i].locations = locations;
+    }
+    console.log("Массив кораблей: ");
+    console.log(this.ships);
+  },
+
+  // метод создает один корабль
+  generateShip: function () {
+    var direction = Math.floor(Math.random() * 2);
+    var row, col;
+
+    if (direction === 1) {
+      // horizontal размещение
+      row = Math.floor(Math.random() * this.boardSize);
+      col = Math.floor(Math.random() * (this.boardSize - this.shipLength + 1));
+    } else {
+      // vertical размещение
+      row = Math.floor(Math.random() * (this.boardSize - this.shipLength + 1));
+      col = Math.floor(Math.random() * this.boardSize);
+    }
+
+    var newShipLocations = [];
+    for (var i = 0; i < this.shipLength; i++) {
+      if (direction === 1) {
+        // добывить в массив для горизонт.
+        newShipLocations.push(row + "" + (col + i));
+      } else {
+        // добавить в массив для вертикал.
+        newShipLocations.push(row + i + "" + col);
+      }
+    }
+    return newShipLocations;
+  },
+
+  // метод получает один корабль и проверяет, что тот не перекрывается с другими кораблями
+  collision: function (locations) {
+    for (var i = 0; i < this.numShips; i++) {
+      var ship = this.ships[i];
+      for (var j = 0; j < locations.length; j++) {
+        if (ship.locations.indexOf(locations[j]) >= 0) {
+          return true;
+        }
+      }
+    }
+    return false;
   },
 };
 // model.fire("23");
@@ -62,6 +121,7 @@ let model = {
 // model.fire("25");
 // model.fire("65");
 // model.fire("43");
+
 let controller = {
   guesses: 0,
   processGuess: function (guess) {
@@ -105,6 +165,8 @@ function init() {
   fireButton.onclick = handleFireButton;
   let guessInput = document.getElementById("guessInput");
   guessInput.onkeypress = handleKeyPress;
+
+  model.generateShipLocations();
 }
 function handleFireButton() {
   let guessInput = document.getElementById("guessInput");
